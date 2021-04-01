4c737a327eb3a49864ed55d3fa6d03a70daec7cd
justDenzil <vikash.denzil@gmail.com>
2021-04-01 13:28:40 +0530

    commit

---
+++ src/pages/lets-talk.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function LetsTalk() {
+  return (
+    <div style={{ color: `black` }}>
+      <h1>Communicate your Ideas to us.</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
